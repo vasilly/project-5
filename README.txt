e14f9b973bf02b5483ecaad1cb1b103cc3b524ed
vasilly <vasko.traikovski@gmail.com>
2015-05-03 11:17:04 +1000

    project-5

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/onge/pen/QwBmmr.
+
+ My first angular.js project with filtering and search, with basic CSS3 animations. What better way to learn that to try and build something...
---
+++ js/index.js
@@ -0,0 +1,92 @@
+// No jQuery!
+
+"use strict";
+
+// Controller
+var images = angular.module("images", ["angular.filter"]);
+
+images.controller("imagesController", function($scope){
+	$scope.list = [
+		{
+			"Title"		: "Stir Fry",
+			"Name"		: "Fried Fresh",
+			"Type"	 	: "Meat",
+			"Mode"	 	: "Dinner",
+			"Image" 	: "food1.jpeg"
+		},
+		{
+			"Title"		: "Lean Beef",
+			"Name"		: "+Healthy Salad",
+			"Type"	 	: "Meat",
+			"Mode"	 	: "Dinner",
+			"Image" 	: "food2.jpeg"
+		},
+		{
+			"Title"		: "Gordon",
+			"Name"		: "Gordon, Australia",
+			"Type"	 	: "Meat",
+			"Mode"	 	: "Dinner",
+			"Image" 	: "food3.jpeg"
+		},
+		{
+			"Title"		: "Sashimi",
+			"Name"		: "Salmon or Tuna",
+			"Type"	 	: "Meat",
+			"Mode"	 	: "Dinner",
+			"Image" 	: "food4.jpeg"
+		},
+		{
+			"Title"		: "Garden Salad",
+			"Name"		: "Healthy",
+			"Type"	 	: "Vegetarian",
+			"Mode"	 	: "Dinner",
+			"Image" 	: "food5.jpeg"
+		},
+		{
+			"Title"		: "Zucchini",
+			"Name"		: "Succulent",
+			"Type"	 	: "Vegetarian",
+			"Mode"	 	: "Lunch",
+			"Image" 	: "food6.jpeg"
+		},
+		{
+			"Title"		: "Buns",
+			"Name"		: "Baked Fresh",
+			"Type"	 	: "Vegetarian",
+			"Mode"	 	: "Lunch",
+			"Image" 	: "food7.jpeg"
+		},
+		{
+			"Title"		: "Sushi",
+			"Name"		: "Fresh",
+			"Type"	 	: "Meat",
+			"Mode"	 	: "Lunch",
+			"Image" 	: "food8.jpeg"
+		},
+		{
+			"Title"		: "Burger",
+			"Name"		: "Wholesome goodness",
+			"Type"	 	: "Meat",
+			"Mode"	 	: "Dinner",
+			"Image" 	: "food9.jpeg"
+		},
+		{
+			"Title"		: "Bread",
+			"Name"		: "Baked Fresh",
+			"Type"	 	: "Meat",
+			"Mode"	 	: "Lunch",
+			"Image" 	: "food10.jpeg"
+		}
+	];
+});
+
+
+// Add/remove classes used in animation
+function filtered(){
+	"use strict";
+	var grid = document.getElementById("images_grid");
+	grid.className = "filter_down"; // Add class that animates out items
+	setTimeout(function(){
+		grid.className = ""; // Remove class to animate items back in
+	},550);
+}
