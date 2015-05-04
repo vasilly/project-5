57bfa158a22dffe9317466157ce03c456ce498a6
vasilly <vasko.traikovski@gmail.com>
2015-05-04 21:16:24 +1000

    project-5

--- js/resize.js
+++ js/resize.js
@@ -37,29 +37,59 @@
 
   document.getElementById("text-sizer").addEventListener("click", function(event) {
     // display the current click count inside the clicked div
+    // var aaa=document.styleSheets[2];
+    // console.log(aaa);
     rootFontSize++;
     if (rootFontSize > 3) rootFontSize = 1;
     // resetFonts(rootFontSize);
-    if (rootFontSize === 3)
-      setClassWidth("card", "100%");
-    else
-		setClassWidth("card", "10rem");
+    if ((rootFontSize === 1) || (rootFontSize === 3))
+      document.body.classList.toggle("overide-card_width");//ie10
+
+    // setClassWidth("card", "100%");
+    // resetWidthCSS( "100%" );
+    // else
+    // setClassWidth("card", "10rem");
+    // resetWidthCSS( "10rem" );
+
 
     document.getElementById('msg1').innerHTML = ("font: " + rootFontSize);
     var root = document.querySelector(":root");
 
     root.style.fontSize = (rootFontSize + "em");
+
+
     // document.body.style.fontSize = (rootFontSize+"rem");
   }, false);
 
 
 
-
   function setClassWidth(klass, width) {
-      var elements = document.getElementsByClassName(klass);
-      for (var i = 0; i < elements.length; i++) {
-        elements[i].style.width = width;
-      }
+    // document.styleSheets[0].cssRules[0].cssText = ".card {width: 100 % !important;}";
+    // 	document.styleSheets[0].cssRules[0].cssText = "\
+    //  #myID {
+    //      myRule: myValue;
+    //      myOtherRule: myOtherValue;
+    //  }";
+    var sheet = document.styleSheets[2]
+    console.log(document.styleSheets[2]);
+    sheet.insertRule(".cwidth { width:100%;}", 0); //IE8
+    //myStyles.deleteRule(0);    IE8
+    var elements = document.getElementsByClassName(klass);
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.width = width;
+    }
+
+    var bdy = document.getElementsByClassName("body");
+
+
+  }
+
+
+
+
+
+  function fullwidth() {
+      // document.styleSheets[2].cssRules[0].cssText = ".card {width: 100 % !important;}";
     }
     // clip to a decent range size to avoid, making endless size adjustments as screens being bigger and bigger
     // var aspect_ratio =  (window.innerHeight/window.innerWidth)/0.75
@@ -74,9 +104,6 @@
   // THUR 10:00 - 23:00
   // FRI to SAT, 10:00 - 23:30
 
-	document.styleSheets[0].cssRules[0].cssText = "\
-	     .card {
-	         width:100%  !important;
-	     }";
+
 
 })();
