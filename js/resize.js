2b9061e2fbe9e6499fce0c4d26d6da1bf4cd0269
vasilly <vasko.traikovski@gmail.com>
2015-05-05 11:19:33 +1000

    project-5

--- js/resize.js
+++ js/resize.js
@@ -1,6 +1,12 @@
 (function() {
   "use strict";
 
+  var
+    ROOT_FONT_SIZE = 1,
+    A1 = document.getElementById('A1'),
+    A2 = document.getElementById('A2'),
+    A3 = document.getElementById('A3');
+
 
   function remCalibrate() {
     // var size = Math.max(16,window.innerWidth/100);
@@ -9,79 +15,63 @@
     // var root = document.querySelector(":root");
 
     document.getElementById('msg1').innerHTML = window.innerWidth;
-    document.getElementById('msg2').innerHTML = window.innerHeight+" : width:";
+    document.getElementById('msg2').innerHTML = window.innerHeight + " : width:";
   }
   remCalibrate();
   window.addEventListener('resize', remCalibrate, false);
-  //-----
-
-
-
-  var rootFontSize = 1
-
-  function changeFontSize() {
-    // var size = Math.max(16,window.innerWidth/100);
-    // document.documentElement.style.fontSize = size + 'px';
-    // console.log('calibrate '+size)
-    // var root = document.querySelector(":root");
-
-  }
-
-  function resetFonts(size) {
-    var A1 = document.getElementById('A1')
-    var A2 = document.getElementById('A2')
-    var A3 = document.getElementById('A3')
-    var sizes = [A1, A2, A3]
-
-  }
+  // remCalibrate
 
   document.getElementById("text-sizer").addEventListener("click", function(event) {
-    // display the current click count inside the clicked div
-    // var aaa=document.styleSheets[2];
-    // console.log(aaa);
-    rootFontSize++;
-    if (rootFontSize > 3) rootFontSize = 1;
-    // resetFonts(rootFontSize);
-    if ((rootFontSize === 1) || (rootFontSize === 3))
-      document.body.classList.toggle("overide-card_width");//ie10
-
-    // setClassWidth("card", "100%");
-    // resetWidthCSS( "100%" );
-    // else
-    // setClassWidth("card", "10rem");
-    // resetWidthCSS( "10rem" );
-
-
-    document.getElementById('msg1').innerHTML = ("font: " + rootFontSize);
+    // 1 = screen
+    // 2 = tablet
+    // 3 = phone
+    ROOT_FONT_SIZE++;
+    if (ROOT_FONT_SIZE > 3) ROOT_FONT_SIZE = 1;
+    if ((ROOT_FONT_SIZE === 1) || (ROOT_FONT_SIZE === 3))
+      document.body.classList.toggle("overide-card_width"); //ie10
     var root = document.querySelector(":root");
-
-    root.style.fontSize = (rootFontSize + "em");
-
-
-    // document.body.style.fontSize = (rootFontSize+"rem");
+    root.style.fontSize = (ROOT_FONT_SIZE + "em");
+    document.getElementById('msg1').innerHTML = ("font: " + ROOT_FONT_SIZE);
+    changePhoneIcon(ROOT_FONT_SIZE);
   }, false);
 
 
-
+  function changePhoneIcon(id) {
+      // A1.style.display = "none";
+      // A2.style.display = "none";
+      // A3.style.display = "none" ;
+      A1.classList.remove("outline");
+      A2.classList.remove("outline");
+      if (id === 1)
+        A1.classList.add("outline");
+      else
+        A2.classList.add("outline");
+        // if(id===1) A1.style.display = "inline";
+        // if(id===2) A2.style.display = "inline";
+        // if(id===3) A3.style.display = "inline";
+    }
+    // changing class attributes in CSS directly was complex and wasn't proceeded with
+    // TODO
   function setClassWidth(klass, width) {
     // document.styleSheets[0].cssRules[0].cssText = ".card {width: 100 % !important;}";
-    // 	document.styleSheets[0].cssRules[0].cssText = "\
+    // document.styleSheets[0].cssRules[0].cssText = "\
     //  #myID {
     //      myRule: myValue;
     //      myOtherRule: myOtherValue;
     //  }";
+
+    //myStyles.deleteRule(0);    IE8
+    // var elements = document.getElementsByClassName(klass);
+    // for (var i = 0; i < elements.length; i++) {
+    //   elements[i].style.width = width;
+    // }
+    // var aaa=document.styleSheets[2];
+    // setClassWidth("card", "100%");
+    // resetWidthCSS( "100%" );
+    // document.body.style.fontSize = (ROOT_FONT_SIZE+"rem");
     var sheet = document.styleSheets[2]
     console.log(document.styleSheets[2]);
     sheet.insertRule(".cwidth { width:100%;}", 0); //IE8
-    //myStyles.deleteRule(0);    IE8
-    var elements = document.getElementsByClassName(klass);
-    for (var i = 0; i < elements.length; i++) {
-      elements[i].style.width = width;
-    }
-
-    var bdy = document.getElementsByClassName("body");
-
-
   }
 
 
@@ -104,8 +94,8 @@
   // THUR 10:00 - 23:00
   // FRI to SAT, 10:00 - 23:30
 
-	window.onload = function() {
-		 document.getElementById("spinner").style.display="none";
-	};
+  window.onload = function() {
+    document.getElementById("spinner").style.display = "none";
+  };
 
 })();
